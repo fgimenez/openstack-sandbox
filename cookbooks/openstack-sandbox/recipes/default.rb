31305e81f1c72ba1dd5d5fc62c9811084667bc28
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-01-12 13:06:12 +0100

    * cookbooks/openstack-sandbox/metadata.rb: supports ubuntu * cookbooks/openstack-sandbox/recipes/default.rb: ruby-shadow for shadowed pass

--- cookbooks/openstack-sandbox/recipes/default.rb
+++ cookbooks/openstack-sandbox/recipes/default.rb
@@ -1,1 +1,11 @@
-#
+package "libshadow-ruby1.8"
+gem "ruby-shadow"
+
+user "openstack" do
+  comment "Openstack User"
+  gid "users"
+  home "/home/openstack"
+  shell "/bin/sh"
+  # password: openstack
+  password "$1$Rt1w56fu$0ELnxGlSofIvZ2OtowS4B."
+end
