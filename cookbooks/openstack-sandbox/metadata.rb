a46823b3f684523bcebe958dfadbc6dbb9da3330
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-01-13 12:28:16 +0100

    Extended metadata; fixed openstack user creation

--- cookbooks/openstack-sandbox/metadata.rb
+++ cookbooks/openstack-sandbox/metadata.rb
@@ -1,1 +1,9 @@
-supports "ubuntu"
+name             "openstack"
+maintainer       "Federico Gimenez Nieto"
+maintainer_email "federico.gimenez@gmail.com"
+license          "Apache 2.0"
+description      "Setup of openstack"
+long_description "Setup of openstack"
+version          "0.0.1"
+recipe           "openstack-sandbox", "Setup of openstack"
+
--- cookbooks/openstack-sandbox/recipes/default.rb
+++ cookbooks/openstack-sandbox/recipes/default.rb
@@ -1,5 +1,11 @@
-package "libshadow-ruby1.8"
-gem "ruby-shadow"
+gem_package "ruby-shadow"
+
+ruby_block "require shadow library" do
+  block do
+    Gem.clear_paths  # <-- Necessary to ensure that the new library is found
+    require 'shadow' # <-- gem is 'ruby-shadow', but library is 'shadow'
+  end
+end
 
 user "openstack" do
   comment "Openstack User"
