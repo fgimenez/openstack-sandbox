ba1c5ab602f97481225c303f5e68774ca674efb1
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-08-01 12:39:15 +0200

    compute configuration complete

--- recipes/configuration.rb
+++ recipes/configuration.rb
@@ -30,3 +30,32 @@ service "mysql" do
   action :restart
 end
 
+#compute
+cookbook_file '/etc/nova/nova.conf' do
+  source 'nova_cfg'
+  owner 'root'
+  group 'root'
+  mode 0644
+  action :create
+end
+
+cookbook_file '/etc/nova/nova-compute.conf' do
+  source 'nova_compute_cfg'
+  owner 'root'
+  group 'root'
+  mode 0644
+  action :create
+end
+
+execute "sync database" do
+  command 'nova-manage db sync'
+end
+
+execute "create private network" do
+  command "nova-manage network create vmnet " +
+    "--fixed_range_v4=10.0.0.0/8 --network_size=64 --bridge_interface=eth2"
+end
+
+execute "create public network" do
+  command 'nova-manage floating create --ip_range=172.16.1.0/24'
+end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -50,4 +50,25 @@ describe 'openstack-sandbox::configuration' do
       expect(runner).to restart_service 'mysql'
     end
   end
+
+  context 'compute' do
+    it_behaves_like "config file creator", '/etc/nova/nova.conf',
+                                           '--dhcpbridge_flagfile=/etc/nova/nova.conf',
+                                           0644
+    it_behaves_like "config file creator", '/etc/nova/nova-compute.conf',
+                                           '--libvirt_type=qemu',
+                                           0644
+    it 'syncs the database' do
+      expect(runner).to execute_command("nova-manage db sync")
+    end
+
+    it 'creates the private network' do
+      expect(runner).to execute_command("nova-manage network create vmnet " +
+        "--fixed_range_v4=10.0.0.0/8 --network_size=64 --bridge_interface=eth2")
+    end
+
+    it 'creates the public network' do
+      expect(runner).to execute_command("nova-manage floating create --ip_range=172.16.1.0/24")
+    end
+  end
 end
